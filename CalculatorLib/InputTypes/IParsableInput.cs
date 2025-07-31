10655fa3c7e983bbc83732356f3780eb5c9c822d
radovand <rdragovic@khaoticen.com>
2025-07-31 15:51:09 +0200

    Code cleanup

--- CalculatorLib/InputTypes/IParsableInput.cs
+++ CalculatorLib/InputTypes/IParsableInput.cs
@@ -1,10 +1,5 @@
 ﻿namespace CalculatorLib.InputTypes;
 
-// public interface IParsableInput
-// {
-//     public static abstract IParsableInput Parse(string input);
-// }
-
 public interface IParsableInput<TSelf> where TSelf : IParsableInput<TSelf>
 {
     static abstract TSelf Parse(string input);
